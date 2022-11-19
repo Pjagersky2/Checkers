a0dc8f0f119046d6ae93b73e37d62098e7402484
Peter Jagersky <pjagersky2@gmail.com>
2022-11-18 22:28:48 -0500

    Made board squares a class object.

--- Checkers/checkers.py
+++ Checkers/checkers.py
@@ -1,15 +1,80 @@
 """Module for displaying a GUI checkerboard."""
 from tkinter import Canvas, Tk
+from typing import Tuple
+
+
+class Square:
+    """Representation of a square on a checkers game."""
+
+    side_length = -1
+    colors = {
+        "dark": "#a77d5c",
+        "light": "#e8cfaa"
+    }
+
+    @classmethod
+    def set_side_length(cls, length: int) -> None:
+        """Set the side length for all square instances."""
+
+        cls.side_length = length
+
+    def __init__(self, col: int, row: int) -> None:
+        """Create a new instance."""
+
+        self.col = col
+        self.row = row
+        self.color = self.get_color()
+
+    def __str__(self) -> str:
+        """String logic."""
+
+        return f"Square(col: {self.col}, row: {self.row}, color: {self.color})"
+
+    @property
+    def coords(self) -> Tuple[int, int, int, int]:
+        """Coordinates of the square."""
+
+        top_left_x = self.col * self.side_length
+        top_left_y = self.row * self.side_length
+
+        bot_right_x = top_left_x + self.side_length
+        bot_right_y = top_left_y + self.side_length
+
+        return (top_left_x, top_left_y, bot_right_x, bot_right_y)
+
+    def get_color(self) -> str:
+        """Evaluate the color."""
+
+        color_index = (self.row + self.col) % 2
+        color_key = "dark" if color_index else "light"
+        color = self.colors[color_key]
+
+        return color
+
+    def draw(self, canvas: Canvas) -> None:
+        """Draw the square to the canvas."""
+
+        canvas.create_rectangle(*self.coords, fill=self.color)
+
+    def is_square(self, col: int, row: int) -> bool:
+        """Check if position is the square."""
+
+        result = False
+        if self.col == col and self.row == row:
+            result = True
+
+        return result
+
+    def reset(self) -> None:
+        """Reset to original square properties."""
+
+        self.color = self.get_color()
 
 
 class Checkers(Canvas):
     """Representation of a checkers game."""
 
     grid_size = 8
-    colors = {
-        "dark": "#a77d5c",
-        "light": "#e8cfaa"
-    }
 
     def __init__(self, master: Tk, size: int = 800) -> None:
         """Create a new instance."""
@@ -19,6 +84,15 @@ class Checkers(Canvas):
         super().__init__(master, width=self.size, height=self.size)
 
         self.bind("<Button-1>", self.left_click)
+
+        self.squares = []
+        for col in range(self.grid_size):
+            for row in range(self.grid_size):
+                square = Square(col, row)
+                self.squares.append(square)
+
+        Square.set_side_length(self.square_length)
+
         self.draw_board()
 
     @property
@@ -27,94 +101,88 @@ class Checkers(Canvas):
 
         return self.size // self.grid_size
 
-    def draw_square(self, col: int, row: int, color: str) -> None:
-        """Draw a square on the canvas."""
+    # def draw_piece(self, col: int, row: int, color: str) -> None:
+    #     """Draw a piece on the canvas."""
 
-        # Top-left corner
-        top_left_x = col * self.square_length
-        top_left_y = row * self.square_length
+    #     reduction_scalar = 0.9  # 90 percent
+    #     offset_percent = (1 - reduction_scalar) / 2
+    #     piece_offset = round(self.square_length * offset_percent)
+    #     reduce_length = self.square_length * reduction_scalar
 
-        #  Bottom-right corner
-        bot_right_x = top_left_x + self.square_length
-        bot_right_y = top_left_y + self.square_length
+    #     # Top-left corner
+    #     top_left_x = (col * self.square_length) + piece_offset
+    #     top_left_y = (row * self.square_length) + piece_offset
 
-        self.create_rectangle(top_left_x,
-                              top_left_y,
-                              bot_right_x,
-                              bot_right_y,
-                              fill=color)
+    #     # Bottom-right corner
+    #     bot_right_x = top_left_x + reduce_length
+    #     bot_right_y = top_left_y + reduce_length
 
-    def draw_piece(self, col: int, row: int, color: str) -> None:
-        """Draw a piece on the canvas."""
+    #     self.create_oval(top_left_x,
+    #                      top_left_y,
+    #                      bot_right_x,
+    #                      bot_right_y,
+    #                      fill=color)
 
-        reduction_scalar = 0.9  # 90 percent
-        offset_percent = (1 - reduction_scalar) / 2
-        piece_offset = round(self.square_length * offset_percent)
-        reduce_length = self.square_length * reduction_scalar
+    def draw_squares(self) -> None:
+        """Draw the squares."""
 
-        # Top-left corner
-        top_left_x = (col * self.square_length) + piece_offset
-        top_left_y = (row * self.square_length) + piece_offset
-
-        # Bottom-right corner
-        bot_right_x = top_left_x + reduce_length
-        bot_right_y = top_left_y + reduce_length
-
-        self.create_oval(top_left_x,
-                         top_left_y,
-                         bot_right_x,
-                         bot_right_y,
-                         fill=color)
+        for square in self.squares:
+            square.draw(self)
 
     def draw_board(self) -> None:
         """Draw the entire checkerboard."""
 
-        for col in range(self.grid_size):
-            for row in range(self.grid_size):
-                color_index = (row + col) % 2
+        self.draw_squares()
 
-                color_key = "dark" if color_index else "light"
-                color = self.colors[color_key]
+        # # Temporary code
+        # for col in range(self.grid_size):
+        #     for row in range(self.grid_size):
+        #         color_index = (row + col) % 2
+        #         color_key = "dark" if color_index else "light"
 
-                self.draw_square(col, row, color)
+        #         if color_key == "dark":
+        #             piece_color = None
 
-                # Temporary code
-                if color_key == "dark":
-                    piece_color = None
+        #             if row < 3:
+        #                 piece_color = "black"
+        #             elif row >= 5:
+        #                 piece_color = "red"
 
-                    if row < 3:
-                        piece_color = "black"
-                    elif row >= 5:
-                        piece_color = "red"
+        #             if piece_color:
+        #                 self.draw_piece(col, row, piece_color)
 
-                    if piece_color:
-                        self.draw_piece(col, row, piece_color)
+    def find_square(self, col: int, row: int) -> Square:
+        """Find a square based on position."""
 
-                # self.draw_piece(col, row, "black")
+        found = None
+        for square in self.squares:
+            if square.is_square(col, row):
+                found = square
+                break
+        else:
+            raise Exception(f"Square not found for position: {col}, {row}")
+
+        return found
+
+    def reset_squares(self) -> None:
+        """Reset all the squares to original properties."""
+
+        for square in self.squares:
+            square.reset()
 
     def left_click(self, event) -> None:
-        """Left-click event handler"""
+        """Left-click event handler."""
 
         col = event.x // self.square_length
         row = event.y // self.square_length
 
-        # (x, y) = event.x , event.y coordinates anywhere on canvas
-        # (a, b) = square grid from 0 - 7
-        # x and y have to be between 0 - 800
-        # assuming x and y are a random point in cell (0, 0)
+        # Change color.
+        square = self.find_square(col, row)
+        print(square)
 
-        # legal random point in cell (0, 0) -> (x, y) = (021, 25)
-        # legal random point in cell (1, 0) -> (x, y) = (125, 25)
-        # legal random point in cell (2, 0) -> (x, y) = (223, 25)
-
-        # cell -> min    - max
-        # 0    -> 0      - 100
-        # 1    -> 100    - 200
-        # 2    -> 200    - 300
-        # given a minimum, to get a cell divide by square_size
-
-        # print("left click", event.x, event.y)
-        print((col, row), (event.x, event.y))
+        square.color = "blue"
+        self.draw_board()
+        self.reset_squares()
 
 
 def main() -> None:
