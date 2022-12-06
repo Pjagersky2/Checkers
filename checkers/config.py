a89d50b90b56a9b492aa556a363137dc259dacec
Peter Jagersky <pjagersky2@gmail.com>
2022-12-05 22:26:05 -0500

    Added more detail to logging configuration.

--- checkers/config.py
+++ checkers/config.py
@@ -1,4 +1,6 @@
 """Module containing the project configuration settings."""
+import logging
+import logging.config
 import os
 
 project_path = os.path.sep.join(__file__.split(os.path.sep)[:-2])
@@ -7,31 +9,78 @@ logdir = os.path.join(project_path, "logs")
 os.makedirs(logdir, exist_ok=True)
 logpath = os.path.join(logdir, "checkers.log")
 
+
+class StdoutFilter(logging.Filter):
+    """Standard output filter."""
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        """Filter for stdout log records."""
+
+        is_info = record.levelno >= logging.INFO
+        is_not_warning = record.levelno < logging.WARNING
+
+        return is_info and is_not_warning
+
+
+class StderrFilter(logging.Filter):
+    """Standard error filter."""
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        """Filter for stderr."""
+
+        return record.levelno >= logging.WARNING
+
+
 logger_config = {
     "version": 1,
     "formatters": {
-        "standard": {
+        "debug": {
+            "format": "%(asctime)s  |  %(levelno)-2d  |  %(name)s  |  %(message)s"
+        },
+        "stdout": {
             "format": "%(message)s"
         },
-        "detailed": {
-            "format": "%(asctime)s [%(levelname)s] %(message)s"
+        "stderr": {
+            "format": "[%(levelname)s] %(message)s"
+        }
+    },
+    "filters": {
+        "stdout": {
+            "()": StdoutFilter
+        },
+        "stderr": {
+            "()": StderrFilter
         }
     },
     "handlers": {
-        "console": {
-            "formatter": "standard",
-            "class": "logging.StreamHandler"
-        },
-        "file": {
-            "formatter": "detailed",
+        "debug": {
+            "formatter": "debug",
             "class": "logging.FileHandler",
-            "filename": logpath
-        }
+            "filename": logpath,
+            "mode": "w",
+            "level": "DEBUG"
+        },
+        "stdout": {
+            "formatter": "stdout",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "filters": ["stdout"]
+        },
+        "stderr": {
+            "formatter": "stderr",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+            "filters": ["stderr"]
+        },
     },
     "loggers": {
+        "__main__": {
+            "level": "DEBUG",
+            "handlers": ["debug", "stdout", "stderr"]
+        },
         "checkers": {
             "level": "DEBUG",
-            "handlers": ["console", "file"]
+            "handlers": ["debug", "stdout", "stderr"]
         }
     }
 }
